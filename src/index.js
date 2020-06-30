c2e4bcd3de20d212ae9a52d73a38883c3be19ffd
Sese <sese.miguel9@gmail.com>
2020-06-30 20:59:28 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function MyInfo(){
+  return (
+    <div>
+      <h1 style={{color: "red"}}>Miguel Karlo Sese</h1>
+      <p>I am learning React JS cause I want to.</p>
+      <ul>
+        <li>Coron</li>
+        <li>Dumaguete</li>
+        <li>Siargao</li>
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(<MyInfo />,document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
